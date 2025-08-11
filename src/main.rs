1a1baa36e324a54618a13b62d6a5776f6e3ac01d
Zebreus <zebreus@zebre.us>
2025-08-11 18:56:00 +0200

    Use why2025-badge-sys

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,55 @@
-#![feature(linkage)]
 #![no_std]
 #![no_main]
 
-// TODO: Rewrite example to do something more interesting
-
-unsafe extern "C" {
-    fn printf(format: *const u8, ...) -> i32;
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn main() -> i32 {
-    unsafe {
-        printf(b"Hello, world! (from rust)\n\0".as_ptr());
-    }
-    let _ = sdl_main();
-    121
-}
+use core::ffi::CStr;
+use sdl3_sys::{
+    error::SDL_GetError,
+    init::{SDL_INIT_VIDEO, SDL_Init},
+    render::{SDL_CreateRenderer, SDL_RenderClear, SDL_RenderPresent, SDL_SetRenderDrawColor},
+    timer::SDL_Delay,
+    video::SDL_CreateWindow,
+};
+use why2025_badge_sys::printf;
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-use core::ffi::CStr;
-use sdl3_sys::{
-    error::SDL_GetError,
-    init::{SDL_Init, SDL_INIT_VIDEO},
-    render::{SDL_CreateRenderer, SDL_RenderClear, SDL_RenderPresent, SDL_SetRenderDrawColor},
-    timer::SDL_Delay,
-    video::SDL_CreateWindow,
-};
+// TODO: Rewrite example to do something more interesting
 
-fn sdl_main() -> Result<(), &'static CStr> {
-    if unsafe { SDL_Init(SDL_INIT_VIDEO) } {
-        unsafe {
-            let window = SDL_CreateWindow(
-                core::mem::transmute("title".as_bytes().as_ptr()),
-                160,
-                160,
-                0,
-            );
-
-            let renderer = SDL_CreateRenderer(window, core::ptr::null_mut());
-            loop {
-                for i in 0..255 {
-                    let (r, g, b, a) = (i, 0, 0, 255);
-                    SDL_SetRenderDrawColor(renderer, r, g, b, a);
-                    SDL_RenderClear(renderer);
-                    SDL_RenderPresent(renderer);
-                    SDL_Delay(50);
-                }
-            }
-        };
-    } else {
-        Err(unsafe { CStr::from_ptr(SDL_GetError()) })
+#[unsafe(no_mangle)]
+pub extern "C" fn main() -> i32 {
+    unsafe {
+        printf(b"Hello, world! (from rust)\n\0".as_ptr());
     }
+    sdl_main();
+    0
+}
+
+fn sdl_main() {
+    unsafe {
+        if !SDL_Init(SDL_INIT_VIDEO) {
+            let message = CStr::from_ptr(SDL_GetError());
+            let mes = message.to_str().unwrap();
+            panic!("{mes}");
+        }
+        let window = SDL_CreateWindow(
+            core::mem::transmute("title".as_bytes().as_ptr()),
+            300,
+            300,
+            0,
+        );
+
+        let renderer = SDL_CreateRenderer(window, core::ptr::null_mut());
+        loop {
+            for i in 0..255 {
+                let (r, g, b, a) = (i, 0, 0, 255);
+                SDL_SetRenderDrawColor(renderer, r, g, b, a);
+                SDL_RenderClear(renderer);
+                SDL_RenderPresent(renderer);
+                SDL_Delay(50);
+            }
+        }
+    };
 }
